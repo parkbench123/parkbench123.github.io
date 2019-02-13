4b381b00c957064a8222328aa5855bbcb128490b
parkbench123 <35406156+parkbench123@users.noreply.github.com>
2019-02-13 12:25:44 -0700

    array for date

--- cit230/assignments/lesson-6/scripts/currentdate.js
+++ cit230/assignments/lesson-6/scripts/currentdate.js
@@ -1,6 +1,34 @@
     //TODO: create scripte to get date fomat
 
+    var today = new Date();
+   
+    //day of week
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+  
+    var dayOfWeek = weekday[today.getDay()];
+
+    //year
+    var fullYear = date.getFullYear();
+
+
+
+    //Month
+
+
+
+
+    //day of Month
+
+
+
     var date = new Date();
 
-    document.getElementById("copyright-year").innerHTML = date.getFullYear();
+    document.getElementById('copyright-year').innerHTML = date.getFullYear();
     
