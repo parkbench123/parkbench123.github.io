9741d5e6fee318a5a81c98e537d3aae0f288499a
parkbench123 <35406156+parkbench123@users.noreply.github.com>
2019-02-20 12:27:31 -0700

    created java wind

--- cit230/assignments/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/scripts/windchill.js
@@ -6,3 +6,5 @@ var windChill = 35.74 + 0.6215 *
     0.4275 * temp * math.pow(windSpeed,0.16);
 
 document.getElementById("temp").innerHTML = temp +'&deg;';
+document.getElementById("windSpeed").innerHTML = windSpeed +'mph';
+document.getElementById("windChill").innerHTML = windChill +'&deg;';
